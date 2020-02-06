ab2de002530474c91a76a2b0290d7eddd281d5c6
aakaspatidar <aakash.patidar@engineerbabu.in>
2020-02-06 17:05:59 +0530

    loader and toast error solved

--- src/app/home/pages/landing/landing.component.ts
+++ src/app/home/pages/landing/landing.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { UsersService } from 'src/app/core/services/users/users.service';
-import { takeUntil, take } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "src/app/core/services/users/users.service";
+import { takeUntil, take } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { ToastrService } from "ngx-toastr";
+import { UtilityService } from "src/app/core/utilities/utility.service";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"]
 })
 export class LandingComponent implements OnInit {
   private _unsubscribe$ = new Subject<boolean>();
   constructor(
-  ) { }
+    private _toastr: ToastrService,
+    private _utility: UtilityService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   ngOnDestroy() {
     this._unsubscribe$.next(true);
     this._unsubscribe$.complete();
+  }
+  showSuccess() {
+    this._toastr.success("Hello world!", "Toastr fun!");
+  }
+
+  showLoader() {
+    this._utility.loaderStart();
   }
 }
