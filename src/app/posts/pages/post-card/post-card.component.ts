e98741a16b141174c8a5c5ea4c1e035f6724ffb5
hitesh0701 <hkarma71@gmail.com>
2020-02-07 19:24:09 +0530

    router navigate

--- src/app/posts/pages/post-card/post-card.component.ts
+++ src/app/posts/pages/post-card/post-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -15,6 +18,7 @@ export class PostCardComponent implements OnInit {
   redirectCardDetail(event){
     event.stopPropogation();
     event.preventDefault();
+    this.router.navigate(['/auth/login']);
   }
   viewUserById(){
     
